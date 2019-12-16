aaac5a527e82414e8eb2362fa36aa17ba9fdf7e3
Chris Padilla <cdpadilla42@gmail.com>
2019-12-16 12:24:24 -0600

    Complete form. Tie to inner array. Render upon submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var x = 3;
+// Add a button on each book’s display to remove the book from the library.
+// You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 
+
+// Debug input for reach checkbox - switch to radio buttons
 var myLibrary = [];
 var display = document.querySelector(".library");
 
@@ -32,10 +35,31 @@ Book.prototype.print = function () {
 pooh.info();
 
 
-function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
+function addBookToLibrary() {
+	let title = document.querySelector("#title");
+	let author = document.querySelector("#author");
+	let pages = document.querySelector("#pages");
+	let read = document.querySelector("#read").value;
+	if (read === "on") {
+		read = true;
+	} else {
+		read = false;
+	}
+	let newBook = new Book(title.value, author.value, pages.value, read);
+	myLibrary.push(newBook);
+	
+	// Clear input field
+	title.value = "";
+	author.value = "";
+	pages.value = "";
+	// Clear button value???
+
+	display.innerHTML = "";
+	render();
 }
+
+var submitBttn = document.querySelector("#submit");
+submitBttn.addEventListener("click", addBookToLibrary);
 
 function render() {
   for (var i = 0; i < myLibrary.length; i++) {
